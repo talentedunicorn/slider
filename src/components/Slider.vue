83b19d72cfed208df52ba4226c348288f48abf9d
Ezekiel Mangalu <emangalu@gmail.com>
2017-12-08 17:03:51 +0800

    Update views and added logo

---
+++ src/components/Slider.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Logo/>
+    <h1>Oi slider here</h1>
+  </div>
+</template>
+
+<script>
+  import Logo from '@/components/Logo';
+
+  export default {
+    name: 'Slider',
+    components: {
+      Logo,
+    },
+  };
+</script>
