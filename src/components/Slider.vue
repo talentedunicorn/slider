5436ca4e07f3829d44a8e4abb3c17112abf21980
Ezekiel Mangalu <emangalu@gmail.com>
2017-12-11 17:41:09 +0800

    Moved Home to pages and create separate slider component

---
+++ src/components/Slider.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>This be the slider</h1>
+</template>
+
+<script>
+  export default {
+    name: 'Slider',
+  };
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -8,15 +8,18 @@
     <Logo class="logo"/>
     <h1>Slider</h1>
     <span>A simple slider with VueJs</span>
+    <Slider/>
   </div>
 </template>
 
 <script>
   import Logo from '@/components/Logo';
+  import Slider from '@/components/Slider';
 
   export default {
     components: {
       Logo,
+      Slider,
     },
   };
 </script>
