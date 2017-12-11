bf532ef1430c85f9ec321a66f14fbef86d242e82
Ezekiel Mangalu <emangalu@gmail.com>
2017-12-12 01:53:42 +0800

    Added styles and prev and next controls

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="slides-wrapper">
-    <ul class="slides">
-      <li class="slide" v-for="(slide, index) in slides" :key="index">
-        <img class="slide-image" :src="slide.url" :alt="slide.text"/>
-      </li>
-    </ul>
-
+  <div>
+    <div class="slides-wrapper">
+      <ul class="slides" :style="{ height: slideHeight, width: slideWidth }">
+        <li class="slide" v-for="(slide, index) in slides" :key="index" :class="{ 'active': current === index }">
+          <img class="slide-image" :src="slide.url" :alt="slide.text"/>
+        </li>
+      </ul>
+    </div>
     <div class="slides-control">
       <a href="#" @click="prev">Prev</a>
       <a href="#" @click="next">Next</a>
@@ -20,30 +21,55 @@
     data() {
       return {
         current: 0,
+        slideHeight: 0,
+        slideWidth: 0,
       };
     },
     methods: {
       prev() {
-        console.log('Show previous'); // eslint-disable-line
+        this.current = this.current > 0 ? this.current - 1 : this.slides.length - 1;
       },
       next() {
-        console.log('Show next'); // eslint-disable-line
+        this.current = this.current < this.slides.length - 1 ? this.current + 1 : 0;
       },
+    },
+    mounted() {
+      const currentEl = document.getElementsByClassName('active')[0].firstElementChild;
+      this.slideHeight = `${currentEl.offsetHeight}px`;
+      this.slideWidth = `${currentEl.offsetWidth}px`;
     },
   };
 </script>
 
 <style lang="scss" scoped>
+  $max-width: 60rem;
+
   .slides {
     list-style: none;
     padding: 0;
     margin: 0;
-    display: flex;
-    flex-flow: row nowrap;
+    position: relative;
+
+    @media screen and (min-width: $max-width) {
+      max-width: 100% !important;
+    }
 
     &-wrapper {
-      width: 50vw;
-      overflow: hidden;
+      margin: 2em 0;
     }
+
+    .slide {
+      visibility: hidden;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+
+      &.active {
+        z-index: 3;
+        visibility: visible;
+      }
+    }
+
   }
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -27,9 +27,9 @@
       return {
         slides: [
           { url: '//picsum.photos/800/600?image=11', text: 'Image 1' },
-          { url: '//picsum.photos/800/600?image=12', text: 'Image 2' },
+          { url: '//picsum.photos/800/500?image=12', text: 'Image 2' },
           { url: '//picsum.photos/800/600?image=13', text: 'Image 3' },
-          { url: '//picsum.photos/800/600?image=14', text: 'Image 4' },
+          { url: '//picsum.photos/1200/600?image=14', text: 'Image 4' },
           { url: '//picsum.photos/800/600?image=15', text: 'Image 5' },
         ],
       };
