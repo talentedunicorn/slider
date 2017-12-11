5914dfe97b8ea98fccef04c48bee8845a6fc2871
Ezekiel Mangalu <emangalu@gmail.com>
2017-12-11 19:00:49 +0800

    Update markup, added controls and test images

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,9 +1,49 @@
 <template>
-  <h1>This be the slider</h1>
+  <div class="slides-wrapper">
+    <ul class="slides">
+      <li class="slide" v-for="(slide, index) in slides" :key="index">
+        <img class="slide-image" :src="slide.url" :alt="slide.text"/>
+      </li>
+    </ul>
+
+    <div class="slides-control">
+      <a href="#" @click="prev">Prev</a>
+      <a href="#" @click="next">Next</a>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'Slider',
+    props: ['slides'],
+    data() {
+      return {
+        current: 0,
+      };
+    },
+    methods: {
+      prev() {
+        console.log('Show previous'); // eslint-disable-line
+      },
+      next() {
+        console.log('Show next'); // eslint-disable-line
+      },
+    },
   };
 </script>
+
+<style lang="scss" scoped>
+  .slides {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-flow: row nowrap;
+
+    &-wrapper {
+      width: 50vw;
+      overflow: hidden;
+    }
+  }
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,13 +2,19 @@
   .logo {
     font-size: 2em;
   }
+
+  .wrapper {
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+  }
 </style>
 <template>
-  <div>
+  <div class="wrapper">
     <Logo class="logo"/>
     <h1>Slider</h1>
     <span>A simple slider with VueJs</span>
-    <Slider/>
+    <Slider :slides="slides"/>
   </div>
 </template>
 
@@ -17,6 +23,17 @@
   import Slider from '@/components/Slider';
 
   export default {
+    data() {
+      return {
+        slides: [
+          { url: '//picsum.photos/800/600?image=11', text: 'Image 1' },
+          { url: '//picsum.photos/800/600?image=12', text: 'Image 2' },
+          { url: '//picsum.photos/800/600?image=13', text: 'Image 3' },
+          { url: '//picsum.photos/800/600?image=14', text: 'Image 4' },
+          { url: '//picsum.photos/800/600?image=15', text: 'Image 5' },
+        ],
+      };
+    },
     components: {
       Logo,
       Slider,
