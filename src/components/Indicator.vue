6614a92938789210a43151d57f00ae32ae93c6e5
Ezekiel Mangalu <emangalu@gmail.com>
2017-12-13 19:16:57 +0800

    Added indicator to slides

---
+++ src/components/Indicator.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="indicator">
+    <svg
+      width="1em"
+      height="1em"
+      viewBox="0 0 20 20"
+      v-for="i in steps"
+      :class="{ 'active': i === current }"
+      :key="i">
+      <circle cx="10" cy="10" r="10"></circle>
+    </svg>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Indicator',
+    props: {
+      steps: {
+        type: Number,
+        required: true,
+      },
+      current: {
+        type: Number,
+        default: 1,
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .indicator {
+    display: flex;
+    flex-flow: row no-wrap;
+    justify-content: center;
+  }
+
+  .active {
+    fill: rgba(0, 0, 0, .8);
+  }
+
+  svg {
+    margin: .5em;
+    fill: rgba(0, 0, 0, .3);
+  }
+</style>
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="slides-wrapper">
+    <Indicator
+      class="slides-indicator"
+      :steps="slides.length"
+      :current="current + 1"/>
     <button
       class="slides-control"
       title= "previous"
@@ -19,18 +23,19 @@
 </template>
 
 <script>
-  const arrowLeft = '<svg viewBox="0 0 20 20" width="1em" height="1em"><path d="M13.891 17.418c0.268 0.272 0.268 0.709 0 0.979s-0.701 0.271-0.969 0l-7.83-7.908c-0.268-0.27-0.268-0.707 0-0.979l7.83-7.908c0.268-0.27 0.701-0.27 0.969 0s0.268 0.709 0 0.979l-7.141 7.419 7.141 7.418z"></path></svg>';
-
-  const arrowRight = '<svg viewBox="0 0 20 20" width="1em" height="1em"><path d="M13.25 10l-7.141-7.42c-0.268-0.27-0.268-0.707 0-0.979 0.268-0.27 0.701-0.27 0.969 0l7.83 7.908c0.268 0.271 0.268 0.709 0 0.979l-7.83 7.908c-0.268 0.271-0.701 0.27-0.969 0s-0.268-0.707 0-0.979l7.141-7.417z"></path></svg>';
+  import Indicator from './Indicator';
 
   export default {
     name: 'Slider',
     props: ['slides', 'prevButton', 'nextButton'],
+    components: {
+      Indicator,
+    },
     data() {
       return {
         current: 0,
-        arrowLeft,
-        arrowRight,
+        arrowLeft: '<svg viewBox="0 0 20 20" width="1em" height="1em"><path d="M13.891 17.418c0.268 0.272 0.268 0.709 0 0.979s-0.701 0.271-0.969 0l-7.83-7.908c-0.268-0.27-0.268-0.707 0-0.979l7.83-7.908c0.268-0.27 0.701-0.27 0.969 0s0.268 0.709 0 0.979l-7.141 7.419 7.141 7.418z"></path></svg>',
+        arrowRight: '<svg viewBox="0 0 20 20" width="1em" height="1em"><path d="M13.25 10l-7.141-7.42c-0.268-0.27-0.268-0.707 0-0.979 0.268-0.27 0.701-0.27 0.969 0l7.83 7.908c0.268 0.271 0.268 0.709 0 0.979l-7.83 7.908c-0.268 0.271-0.701 0.27-0.969 0s-0.268-0.707 0-0.979l7.141-7.417z"></path></svg>',
       };
     },
     methods: {
@@ -59,10 +64,17 @@
       display: flex;
       flex-flow: row nowrap;
       justify-content: center;
+      position: relative;
+    }
+
+    &-indicator {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
     }
 
     &-control {
-      background: rgba(#AAA, .2);
+      background: rgba(#aaa, .2);
       border: none;
       outline: none;
       font-size: 2em;
